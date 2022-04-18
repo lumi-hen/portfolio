a181aa968d19381b1cd0400104019079027eb8f5
unknown <lumihennig@gmail.com>
2022-04-18 16:37:53 +0200

    db

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const Page = require('./models/page');
 const Category = require('./models/category');
 
 // Connect to db
-mongoose.connect(config.localhost, {
+mongoose.connect(config.database, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
