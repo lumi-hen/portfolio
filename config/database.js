a409525bbc48fa217ee43acbcff503ad760f541c
unknown <lumihennig@gmail.com>
2022-04-18 15:46:11 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-  database: 'mongodb://127.0.0.1:27017/portfolio',
+  localhost: 'mongodb://127.0.0.1:27017/portfolio',
+
+  database: 'mongodb+srv://doadmin:Wa31x7dP8ku64I02@db-mongodb-ams3-02325-4a31b711.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-ams3-02325&tls=true&tlsCAFile=C:\Users\Lumi\Desktop\portfolio\config\ca-certificate.crt',
 }
