e2ecf7f9500209444d251996a19274e9fc1176a7
unknown <lumihennig@gmail.com>
2022-04-18 17:46:43 +0200

    db

--- config/database.js
+++ config/database.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   
 
-  database: 'mongodb+srv://doadmin:Wa31x7dP8ku64I02@db-mongodb-ams3-02325-4a31b711.mongo.ondigitalocean.com/portfolio?authSource=admin&replicaSet=db-mongodb-ams3-02325&tls=true&tlsCAFile=/portfolio/config/ca-certificate.crt',
+  database: 'mongodb+srv://doadmin:Wa31x7dP8ku64I02@db-mongodb-ams3-02325-4a31b711.mongo.ondigitalocean.com/portfolio?authSource=admin&replicaSet=db-mongodb-ams3-02325&tls=true&tlsCAFile=/root/portfolio/config/ca-certificate.crt',
 
 }
