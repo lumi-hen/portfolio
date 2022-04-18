ef3504b3d379ba6968ec6c386825f13912b551e2
unknown <lumihennig@gmail.com>
2022-04-18 16:56:04 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   localhost: 'mongodb://127.0.0.1:27017/portfolio',
 
-  database: 'mongodb+srv://doadmin:<replace-with-your-password>@db-mongodb-ams3-02325-4a31b711.mongo.ondigitalocean.com/portfolio?authSource=admin&replicaSet=db-mongodb-ams3-02325&tls=true&tlsCAFile=/root/portfolio/config/ca-certificate.crt',
-  
+  database: 'mongodb+srv://doadmin:Wa31x7dP8ku64I02@db-mongodb-ams3-02325-4a31b711.mongo.ondigitalocean.com/portfolio?authSource=admin&replicaSet=db-mongodb-ams3-02325&tls=true&tlsCAFile=/root/portfolio/config/ca-certificate.crt',
+
 }
